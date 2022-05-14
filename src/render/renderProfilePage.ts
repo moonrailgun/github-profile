3d0380ebd423e1bbe9eedb704cef5728f58e1a8d
moonrailgun <moonrailgun@gmail.com>
2022-05-14 18:50:04 +0800

    fix: fix new version github page adapter

--- src/render/renderProfilePage.ts
+++ src/render/renderProfilePage.ts
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 
 export function renderProfilePage(username: string) {
   const targetNode = document.querySelector(
-    '#js-pjax-container > div:nth-child(2) > div > div:nth-child(2)'
+    '#js-pjax-container > div:nth-last-child(1) > div > div:nth-child(2)'
   );
 
   ReactDOM.render(React.createElement(ProfilePage, { username }), targetNode);
